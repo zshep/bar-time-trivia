bae47a6b7fa842a011be52a0669d2d541e0d2b31
zsheperdigian <zsheperdigian@gmail.com>
2024-09-07 14:54:14 -0700

    attemptig to add actions to cause create session to work

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,20 @@
 import { z } from 'zod';
 
 
+export type State = {
+    errors?: {
+      joincode?: string[];
+      
+    };
+    message?: string | null;
+  };
+
+
 // make action to create Session
+export async function createSession(prevState: State, formdata: FormData) {
+
+
+
+}
 
 
