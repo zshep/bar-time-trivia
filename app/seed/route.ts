76e9a091cf7fd927d991dbe12ee9a24e1b33b3bb
zsheperdigian <zsheperdigian@gmail.com>
2024-09-18 12:57:41 -0700

    committing db stuff

---
+++ app/seed/route.ts
@@ -0,0 +1,19 @@
+import bcrypt from 'bcrypt';
+import { db } from '@vercel/postgres';
+
+cosnt client = await db.connect();
+
+//creating schemas for tables
+
+async function createUsers() {
+
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+    await client.sql`
+        CREATE TABLE IF NOT EXISTS users (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        username VARCHAR(255) NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL
+        );
+    `;
+}
