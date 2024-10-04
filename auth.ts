937abedf6952e2c522b44883524c0006842abd2b
zsheperdigian <zsheperdigian@gmail.com>
2024-10-04 15:51:29 -0700

    finally fixed the missing secret issue. Now need to make sure log in works

--- auth.ts
+++ auth.ts
@@ -7,38 +7,38 @@ import type { User } from './app/lib/definitions';
 import bcrypt from 'bcrypt';
 
 async function getUser(email: string): Promise<User | undefined> {
-    try {
-      const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-      return user.rows[0];
-    } catch (error) {
-      console.error('Failed to fetch user:', error);
-      throw new Error('Failed to fetch user.');
-    }
+  try {
+    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+    return user.rows[0];
+  } catch (error) {
+    console.error('Failed to fetch user:', error);
+    throw new Error('Failed to fetch user.');
   }
+}
 
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: [
-    Credentials ({
-        async authorize(credentials) {
-            const parsedCredentials = z
-            .object({email: z.string().email(), password: z.string().min(3) })
-            .safeParse(credentials);
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(3) })
+          .safeParse(credentials);
 
-            if (parsedCredentials.success) {
-                const { email, password } = parsedCredentials.data;
-                const user = await getUser(email);
-                if (!user) return null;
-                const passwordsMatch = await bcrypt.compare(password, user.password);
-                
-                if (passwordsMatch) return user;
-            }
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
 
-            console.log('Invalid credentials');
-            return null;
+          if (passwordsMatch) return user;
+        }
 
-        },
+        console.log('Invalid credentials');
+        return null;
+
+      },
     }),
-],
+  ],
 });
